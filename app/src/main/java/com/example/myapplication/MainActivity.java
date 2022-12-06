3b87947ee2810a542f39076779bf648d06d156a6
tchitanava1 <nika.tchitanava.1@btu.edu.ge>
2022-12-07 00:17:03 +0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.widget.TableLayout;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        tabLayout = findViewById(R.id.tablayout);
+        viewPager = findViewById(R.id.viewpager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new fragment1(), "First");
+        vpAdapter.addFragment(new fragment2(), "Second");
+        vpAdapter.addFragment(new fragment3(), "Third");
+        vpAdapter.addFragment(new fragment4(), "Fourth");
+        viewPager.setAdapter(vpAdapter);
+    }
+
+    }
